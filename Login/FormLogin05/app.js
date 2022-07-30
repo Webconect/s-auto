6ee7bcad9d6fde298cd8b5277af9317a8d5dccb9
Webconect <106142081+Webconect@users.noreply.github.com>
2022-07-30 14:20:59 +0700

    upgrade

---
+++ Login/FormLogin05/app.js
@@ -0,0 +1,6 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const container = document.querySelector(".container");
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-in-mode");
+});
